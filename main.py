1d0bb568a3363577cfff443ae92d59794adc62b9
atharva-aiml <atharva.ai1509@gmail.com>
2025-08-03 20:39:45 +0530

    0307

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
+from network_security.components.data_validation import DataValidation
 from network_security.components.data_ingestion import DataIngestion
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
-from network_security.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from network_security.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
 
 if __name__ == '__main__':
     try:
@@ -12,5 +13,9 @@ if __name__ == '__main__':
         logging.info("enter try block")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
         print(dataingestionartifact)
+        datavalidationconfig = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, datavalidationconfig)
+        data_validation_artifact = data_validation.initiate_data_validation()
+        print(data_validation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
