d11f0ec0dd30c5c4712a110d9aaa2fd84777f2fd
atharva-aiml <atharva.ai1509@gmail.com>
2025-08-04 23:59:37 +0530

    adf

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 from network_security.components.data_validation import DataValidation
 from network_security.components.data_ingestion import DataIngestion
+from network_security.components.data_transformation import DataTransformation
+from network_security.components.model_trainer import ModelTrainer
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
-from network_security.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
+from network_security.entity.config_entity import DataIngestionConfig, ModelTrainerConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
 
 if __name__ == '__main__':
     try:
@@ -17,5 +19,18 @@ if __name__ == '__main__':
         data_validation = DataValidation(dataingestionartifact, datavalidationconfig)
         data_validation_artifact = data_validation.initiate_data_validation()
         print(data_validation_artifact)
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
+
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
